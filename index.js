55330aa6ee2e28681c064df13b9b850983b8e318
Hassen Hassen <xasansq@gmail.com>
2022-12-02 17:02:58 -0600

    Prompt added

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const fs = require('fs');
 const dir = './data/left';
+const prompt = require('prompt-sync')();
 
-const fileNames = fs.readdirSync("./data/left")
+const pwd = prompt('Name of directory you want to alter: ');
 
-fs.readdir(dir, (err, files) => {
-    console.log(files.length);
-    let names = [], i = 0;
-    while(names.length < files.length){
-        var r = Math.floor(Math.random() * files.length) + 1;
-        if(names.indexOf(r) === -1) names.push(r);
-    }
-    console.log(names)
+const fileNames = fs.readdirSync(`./data/${pwd}`)
+
+function changeFileNames() {
+    fs.readdir(dir, (err, files) => {
+        console.log(files.length);
+        let names = [], i = 0;
+        while (names.length < files.length) {
+            var r = Math.floor(Math.random() * files.length) + 1;
+            if (names.indexOf(r) === -1) names.push(r);
+        }
+        console.log(names)
 
 
-    for(let fls of fileNames){
-      //console.log(fls)
-      let oldName = `./data/left/${fls}`
-      let newName = `./data/left/${names[i]}.gif`
-      fs.rename(oldName, newName, err => {
-          if (err) {
-            throw err
-          }
-        
-          console.log('Directory renamed successfully.')
-        })
-      i++
-    }
+        for (let fls of fileNames) {
+            let oldName = `./data/${pwd}/${fls}`
+            let newName = `./data/${pwd}/${names[i]}.gif`
+            fs.rename(oldName, newName, err => {
+                if (err) {
+                    throw err
+                }
 
-});
+                console.log(`Renamed ${fls} to ${newName} successfully.`)
+            })
+            i++
+        }
+    });
+}
+
+changeFileNames()
+
 
 
 
