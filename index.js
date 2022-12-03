0d124aef81ec1fc61755e137ebd87b96defc7396
Hassen Hassen <xasansq@gmail.com>
2022-12-02 18:06:25 -0600

    remove

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const dir = './data/left';
 const prompt = require('prompt-sync')();
 
 const pwd = prompt('Name of directory you want to alter: ');
-
 const fileNames = fs.readdirSync(`./data/${pwd}`)
+const pseudoFileNames = fs.readdirSync(`./data/pseudo${pwd}`)
+
+for(let fls of pseudoFileNames){
+    fs.copyFile(`./data/pseudo${pwd}/${fls}`, `./data/${pwd}/${fls}`, (err) => {
+        if(err) {
+            console.log(err.message)
+        }else {
+            console.log("done")
+        }
+    })
+}
+
 
 function changeFileNames() {
     fs.readdir(dir, (err, files) => {
-        console.log(files.length);
         let names = [], i = 0;
         while (names.length < files.length) {
             var r = Math.floor(Math.random() * files.length) + 1;
             if (names.indexOf(r) === -1) names.push(r);
         }
-        console.log(names)
-
 
         for (let fls of fileNames) {
             let oldName = `./data/${pwd}/${fls}`
