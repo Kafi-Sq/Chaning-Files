65fbc403ccb1052d6d17e0a6492acbb27cccbfb4
Hassen Hassen <xasansq@gmail.com>
2022-12-04 11:17:33 -0600

    Working

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const dir = './data/left';
 const prompt = require('prompt-sync')();
 
-const pwd = prompt('Name of directory you want to alter: ');
+const pwd = prompt('Name of directory you want to alter(q to quit): ');
 const fileNames = fs.readdirSync(`./data/${pwd}`)
+const dir = `./data/${pwd}`;
 const pseudoFileNames = fs.readdirSync(`./data/pseudo${pwd}`)
 
 
-function reverseBack(){
+async function reverseBack() {
     for (let fls of fileNames) {
         try {
             fs.unlinkSync(`./data/${pwd}/${fls}`);
@@ -24,22 +24,21 @@ function reverseBack(){
     }
 }
 
-
-function changeFileNames() {
-    reverseBack()
+async function changeFileNames() {
     fs.readdir(dir, (err, files) => {
         let names = [], i = 0;
         while (names.length < files.length) {
             var r = Math.floor(Math.random() * files.length) + 1;
             if (names.indexOf(r) === -1) names.push(r);
         }
+        console.log(names)
 
         for (let fls of fileNames) {
             let oldName = `./data/${pwd}/${fls}`
             let newName = `./data/${pwd}/${names[i]}.gif`
             fs.rename(oldName, newName, err => {
                 if (err) {
-                    console.log("Reversed back to original, input this directory again to randomize.")
+                    console.log(err.message)
                 }
 
                 console.log(`Renamed ${fls} to ${newName} successfully.`)
@@ -48,9 +47,20 @@ function changeFileNames() {
         }
         names = []
     });
+
+    console.log("done")
+
 }
 
-changeFileNames()
+if (fileNames[0] === "1.gif") {
+    reverseBack()
+}else{
+    changeFileNames()
+}
+
+
+
+
 
 
 
